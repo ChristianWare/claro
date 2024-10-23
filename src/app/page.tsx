ae11d26c6cf66381e6bbffb28851d9583890b69e
Chris Ware <c.p.ware@hotmail.com>
2024-10-23 01:08:58 -0700

    Usp Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Hero from "@/components/Hero/Hero";
 import Popular from "@/components/Popular/Popular";
+import Usp from "@/components/Usp/Usp";
 
 const HomePage = async () => {
   return (
     <main>
       <Hero />
       <Popular />
+      <Usp />
+      <Usp />
+      <Usp />
     </main>
   );
 };
