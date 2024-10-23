61c8667abac4403cb0f5f4af0bdeac181b0b522f
Chris Ware <c.p.ware@hotmail.com>
2024-10-23 02:22:52 -0700

    Added play/pause button

--- src/components/VideoUsp/VideoUsp.tsx
+++ src/components/VideoUsp/VideoUsp.tsx
@@ -1,10 +1,36 @@
+'use client'
+
+import { useState, useRef } from "react";
 import styles from "./VideoUsp.module.css";
+import Pause from "../../../public/icons/pause.svg";
+import Play from "../../../public/icons/play.svg";
 
 const VideoUsp = () => {
+  const videoRef = useRef<HTMLVideoElement>(null); // Ref to control video
+  const [isPlaying, setIsPlaying] = useState(true); // Track play/pause state
+
+  const handlePlayPause = () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        videoRef.current.pause(); // Pause video
+      } else {
+        videoRef.current.play(); // Play video
+      }
+      setIsPlaying(!isPlaying); // Toggle play/pause state
+    }
+  };
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <video preload='auto' autoPlay muted loop className={styles.video}>
+        <video
+          ref={videoRef}
+          preload='auto'
+          autoPlay
+          muted
+          loop
+          className={styles.video}
+        >
           <source src='./video/lossless.mp4' />
         </video>
         <div className={styles.imgOverlay}></div>
@@ -16,8 +42,18 @@ const VideoUsp = () => {
             Lossless audio like you&apos;ve <br /> never experienced{" "}
           </h2>
         </div>
+
+        {/* Play/Pause Button */}
+        <button onClick={handlePlayPause} className={styles.playPauseButton}>
+          {isPlaying ? (
+            <Pause width={25} height={25} />
+          ) : (
+            <Play width={25} height={25} />
+          )}
+        </button>
       </div>
     </section>
   );
 };
+
 export default VideoUsp;
