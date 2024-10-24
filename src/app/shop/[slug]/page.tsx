3629473ee1df4aa18c7bedd306403e4514a516b3
Chris Ware <c.p.ware@hotmail.com>
2024-10-23 19:57:46 -0700

    c

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -7,8 +7,41 @@ import DOMPurify from "isomorphic-dompurify";
 import ProductImages from "@/components/ProductImages/ProductImages";
 import SlugIntro from "@/components/SlugIntro/SlugIntro";
 import GalleryGrid from "@/components/GalleryGrid/GalleryGrid";
-
+import Calendar from "../../../../public/icons/calendar.svg";
+import Rotate from "../../../../public/icons/rotate.svg";
+import Mailbox from "../../../../public/icons/mailbox.svg";
+import Globe from "../../../../public/icons/globe.svg";
 // import { Suspense } from "react";
+
+interface DataItem {
+  id: number;
+  icon: JSX.Element;
+  text: string;
+}
+
+
+const data = [
+  {
+    id: 1,
+    icon: <Calendar width={50} height={50} className={styles.icon} />,
+    text: "90 Day Guarantee",
+  },
+  {
+    id: 2,
+    icon: <Rotate width={50} height={50} className={styles.icon} />,
+    text: "30 Day Returns",
+  },
+  {
+    id: 3,
+    icon: <Mailbox width={50} height={50} className={styles.icon} />,
+    text: "Ships Next Day",
+  },
+  {
+    id: 4,
+    icon: <Globe width={50} height={50} className={styles.icon} />,
+    text: "Ships Globally",
+  },
+];
 
 const SlugPage = async ({ params }: { params: { slug: string } }) => {
   console.log(params.slug);
@@ -62,6 +95,20 @@ const SlugPage = async ({ params }: { params: { slug: string } }) => {
         <br />
         <br />
         <GalleryGrid items={product.media?.items} />
+        <br />
+        <br />
+        <div className={styles.parent}>
+          <LayoutWrapper>
+            <div className={styles.bottomii}>
+              {data.map((x: DataItem) => (
+                <div key={x.id} className={styles.box}>
+                  {x.icon}
+                  <p className={styles.text}>{x.text}</p>
+                </div>
+              ))}
+            </div>
+          </LayoutWrapper>
+        </div>
       </LayoutWrapper>
     </main>
   );
